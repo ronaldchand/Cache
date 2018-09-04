d024ad61af832b12002094f9278355c3b2d13931
Ronald Chand <ronald.chand@vocusgroup.co.nz>
2018-09-04 14:55:33 +1200

    Mem cache implementation

---
+++ MemCache/MemCache.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading;
+
+namespace MemCache
+{
+    /// <summary>
+    /// Mem cache that implements a set number of records that can be held in memory. Once that
+    /// limit is exceeded the oldest data cached will be removed to make space for the new one.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <seealso cref="MemCache.ICache{TKey, TValue}" />
+    public class MemCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        private readonly int _maxNumberOfRecordsToStore;
+
+        private readonly OrderedDictionary _orderedDictionaryCache = new OrderedDictionary();
+        private readonly ReaderWriterLockSlim _readerWriterLocker = new ReaderWriterLockSlim();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemCache{TKey, TValue}"/> class.
+        /// </summary>
+        /// <param name="maxNumberOfRecordsToStore">The number of records to store.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public MemCache(int maxNumberOfRecordsToStore)
+        {
+            if (maxNumberOfRecordsToStore <= 0) throw new ArgumentException($"Maximum number of records to store must greater than zero");
+            _maxNumberOfRecordsToStore = maxNumberOfRecordsToStore;
+        }
+
+        /// <summary>
+        /// Adds the value to the cache against the specified key.
+        /// If the key already exists, its value is updated.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void AddOrUpdate(TKey key, TValue value)
+        {
+            _readerWriterLocker.EnterWriteLock();
+            try
+            {
+                if (!_orderedDictionaryCache.Contains(key))
+                {
+                    if (_orderedDictionaryCache.Count >= _maxNumberOfRecordsToStore)
+                    {
+                        //remove oldest record if the capacity has been reached
+                        _orderedDictionaryCache.RemoveAt(0); 
+                    }
+                    _orderedDictionaryCache.Add(key, value);
+                }
+                else
+                {
+                    //removes the entry and adds it to the bottom of the list to ensure that 
+                    //any data that gets updated relocated in the dictionary.
+                    _orderedDictionaryCache.Remove(key);
+                    _orderedDictionaryCache.Add(key, value);
+                }
+            }
+            finally
+            {
+                _readerWriterLocker.ExitWriteLock();
+            }
+        }
+
+        /// <summary>
+        /// Attempts to gets the value from the cache against the specified key
+        /// and returns true if the key existed in the cache.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var valueFoundInCache = false;
+            value = default(TValue);
+
+            _readerWriterLocker.EnterReadLock();
+            try
+            {
+                if (_orderedDictionaryCache.Contains(key))
+                {
+                    value = (TValue) _orderedDictionaryCache[key];
+                    valueFoundInCache = true;
+                }
+            }
+            catch (Exception)
+            {
+                value = default(TValue);
+            }
+            finally
+            {
+                _readerWriterLocker.ExitReadLock();
+            }
+
+            return valueFoundInCache;
+        }       
+    }
+}
---
+++ UnitTests/MenCacheTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using MemCache;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class MenCacheTests
+    {
+        private MemCache<string, string> _memCache;
+
+        [SetUp]
+        public void Init()
+        {
+            _memCache = new MemCache<string,string>(2); 
+        }
+        
+        [Test]
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void MemCache_InvalidMaxNumberOfRecordsToStoreValue_ShouldThrowArgumentException(int num)
+        {
+            Assert.Throws<ArgumentException>(() => new MemCache<string, object>(num));
+        }
+
+        [Test]
+        public void AddOrUpdate_NullKeyValuePassed_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => _memCache.AddOrUpdate(null, "somedata"));
+        }
+
+        [Test]
+        [TestCase("NullValue", null)]
+        [TestCase("ActualValue", "testing")]
+        public void AddOrUpdate_ValuePassedIn_ShouldAddToCache(string key, string value)
+        {
+            _memCache.AddOrUpdate(key, value);
+            _memCache.TryGetValue(key, out var dataAdded);
+            Assert.AreEqual(value, dataAdded);
+        }
+
+        [Test]
+        public void AddOrUpdate_EntryExceedsLimit_ShouldRemoveTheOldestFromTheList()
+        {
+            _memCache.AddOrUpdate("firstKey", "firstValue");
+            _memCache.AddOrUpdate("secondKey", "secondValue");
+            _memCache.AddOrUpdate("thirdKey", "thirdValue");
+            
+            Assert.IsFalse(_memCache.TryGetValue("firstKey", out var dataNotFound));
+            Assert.IsTrue(_memCache.TryGetValue("secondKey", out var secondValue));
+            Assert.IsTrue(_memCache.TryGetValue("thirdKey", out var thirdValue));
+        }
+
+        [Test]
+        public void AddOrUpdate_UpdateTheOldestAndExceedValue_ShouldNotRemoveTheUpdatedValue()
+        {
+            _memCache.AddOrUpdate("firstKey", "firstValue");
+            _memCache.AddOrUpdate("secondKey", "secondValue");
+            _memCache.AddOrUpdate("firstKey", "newFirstKeyValue");
+            _memCache.AddOrUpdate("thirdKey", "thirdKeyValue");
+
+            _memCache.TryGetValue("firstKey", out var firstValue);
+            Assert.IsFalse(_memCache.TryGetValue("secondKey", out var secondKey));
+            Assert.AreEqual("newFirstKeyValue", firstValue);
+        }
+
+        //[Test]
+        //public void TryGetValue_InvalidOutValueDefined_ShouldReturnFalseAndDefaultValue()
+        //{
+        //    _memCache.AddOrUpdate("firstKey", 1);
+
+        //    var result = _memCache.TryGetValue("firstKey", out string value);
+        //}
+    }
+}
